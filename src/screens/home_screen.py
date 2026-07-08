999d78c72623eae9e1107d49ca476244c92b73be
sy imran <is0981102@gmail.com>
2026-07-08 23:48:17 +0530

    Add main application logic and screen components for user login

---
+++ src/screens/home_screen.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from src.components.header import header_home
+from src.ui.base_layout import style_base_layout, style_background_home
+
+def home_screen():
+
+    style_base_layout()
+    header_home()
+    style_background_home()
+    col1,col2 = st.columns(2)
+
+    with col1:
+        if st.button('Teacher Portal'):
+            st.session_state['login_type'] = 'teacher'
+            st.rerun()
+
+    with col2:
+        if st.button('Student Portal'):
+            st.session_state['login_type'] = 'student'
+            st.rerun()
