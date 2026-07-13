a165c954ac43947ee1b2863c9378b3c4b5f8c586
sy imran <is0981102@gmail.com>
2026-07-13 18:15:25 +0530

    Integrate face embedding, SVM classifier, and voice embedding pipeline

--- src/database/db.py
+++ src/database/db.py
@@ -23,3 +23,12 @@ def teacher_login(username, password):
         if check_pass(password, teacher["password"]):
             return teacher
     return None
+
+def get_all_students():
+    response = supabase.table("students").select("*").execute()
+    return response.data
+
+def create_student(new_name, face_embedding=None, voice_embedding=None):
+    data = {'name': new_name, 'face_embedding':face_embedding, 'voice_embedding':voice_embedding}
+    response =supabase.table('students').insert(data).execute()
+    return response.data
